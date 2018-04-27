1d95cd7a5cedc6a4824ff3f77ae1caa3b5fd994c
Ryszard Balcerzak <rysz.b82@gmail.com>
2018-04-27 18:32:40 +0200

    Added Gesture ValueObject

---
+++ RockPaperScissors/RockPaperScissors.Logic/Gesture.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RockPaperScissors.Logic
+{
+    public class Gesture : ValueObject<Gesture>
+    {
+        public static readonly Gesture Rock = new Gesture(1, "Rock");
+        public static readonly Gesture Paper = new Gesture(2, "Paper");
+        public static readonly Gesture Scissors = new Gesture(3, "Scissors");
+        public static readonly Gesture Lizard = new Gesture(5, "Lizard");
+        public static readonly Gesture Spock = new Gesture(4, "Spock");
+
+        public int Idx { get; }
+
+        public string Name { get; }
+
+        private Gesture(int idx, string name)
+        {
+            Idx = idx;
+            Name = name;
+        }
+
+        public static bool operator >(Gesture gestureA, Gesture gestureB)
+        {
+            return gestureA != gestureB && IsBigger(gestureA.Idx, gestureB.Idx);
+        }
+
+        public static bool operator <(Gesture gestureA, Gesture gestureB)
+        {
+            return gestureA != gestureB && !IsBigger(gestureA.Idx, gestureB.Idx);
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Idx;
+        }
+
+        private static bool IsBigger(int x, int y)
+        {
+            var maxIdx = Math.Max(x, y);
+            var upperBound = IsOdd(maxIdx) ? maxIdx : maxIdx + 1;
+
+            for (var i = 1; i <= upperBound / 2; i++)
+            {
+                if (y == (x + 2 * i - 1) % upperBound + 1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool IsOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+    }
+}
---
+++ RockPaperScissors/RockPaperScissors.Tests/GestureTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using RockPaperScissors.Logic;
+
+using static RockPaperScissors.Logic.Gesture;
+
+namespace RockPaperScissors.Tests
+{
+    [TestFixture]
+    public class GestureTests
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(Scissors, Paper).Returns(true);
+                yield return new TestCaseData(Paper, Rock).Returns(true);
+                yield return new TestCaseData(Rock, Lizard).Returns(true);
+                yield return new TestCaseData(Lizard, Spock).Returns(true);
+                yield return new TestCaseData(Spock, Scissors).Returns(true);
+                yield return new TestCaseData(Scissors, Lizard).Returns(true);
+                yield return new TestCaseData(Lizard, Paper).Returns(true);
+                yield return new TestCaseData(Paper, Spock).Returns(true);
+                yield return new TestCaseData(Spock, Rock).Returns(true);
+                yield return new TestCaseData(Rock, Scissors).Returns(true);
+            }
+        }
+
+        [Test]
+        [TestCaseSource(typeof(GestureTests), nameof(TestCases))]
+        public bool GreaterThanOperator_Should_CorreclyCompareGestures_When_GesturesArentEqual(
+            Gesture gestureA,
+            Gesture gestureB)
+        {
+            return gestureA > gestureB;
+        }
+
+        [Test]
+        [TestCaseSource(typeof(GestureTests), nameof(TestCases))]
+        public bool LessThanOperator_Should_CorreclyCompareGestures_When_GesturesArentEqual(
+            Gesture gestureA,
+            Gesture gestureB)
+        {
+            return !(gestureA < gestureB);
+        }
+    }
+}
