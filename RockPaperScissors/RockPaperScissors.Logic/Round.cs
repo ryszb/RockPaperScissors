34d9579ac2e09ae16d9a003c3a97f5829b4db95a
Ryszard Balcerzak <rysz.b82@gmail.com>
2018-04-27 18:34:46 +0200

    Added Round ValueObject

---
+++ RockPaperScissors/RockPaperScissors.Logic/Round.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+using static RockPaperScissors.Logic.RoundResult;
+
+namespace RockPaperScissors.Logic
+{
+    public class Round : ValueObject<Round>
+    {
+        public Gesture PlayerOneGesture { get; }
+
+        public Gesture PlayerTwoGesture { get; }
+
+        public Round(Gesture playerOneGesture, Gesture playerTwoGesture)
+        {
+            PlayerOneGesture = playerOneGesture;
+            PlayerTwoGesture = playerTwoGesture;
+        }
+
+        public RoundResult FinishRound()
+        {
+            if (PlayerOneGesture > PlayerTwoGesture)
+            {
+                return PlayerOneWins;
+            }
+
+            if (PlayerOneGesture < PlayerTwoGesture)
+            {
+                return PlayerTwoWins;
+            }
+
+            return Draw;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return PlayerOneGesture;
+            yield return PlayerTwoGesture;
+        }
+    }
+
+    public enum RoundResult
+    {
+        Draw,
+        PlayerOneWins,
+        PlayerTwoWins
+    }
+}
---
+++ RockPaperScissors/RockPaperScissors.Tests/RoundTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+using NUnit.Framework;
+
+using RockPaperScissors.Logic;
+
+using static RockPaperScissors.Logic.Gesture;
+using static RockPaperScissors.Logic.RoundResult;
+
+namespace RockPaperScissors.Tests
+{
+    [TestFixture]
+    public class RoundTests
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(Paper, Scissors).Returns(PlayerTwoWins);
+                yield return new TestCaseData(Paper, Rock).Returns(PlayerOneWins);
+                yield return new TestCaseData(Rock, Rock).Returns(Draw);
+            }
+        }
+
+        [Test]
+        [TestCaseSource(typeof(RoundTests), nameof(TestCases))]
+        public RoundResult FinishRound_Should_CorreclyReturnResult(Gesture gestureA, Gesture gestureB)
+        {
+            return new Round(gestureA, gestureB).FinishRound();
+        }
+    }
+}
